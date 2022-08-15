ccbdb6db1502aff5b34efc54ef25183a1f3629e9
irsc <iresearch.rad@gmail.com>
2022-08-15 22:11:10 +0700

    update form foto

--- resources/views/web/input-data/usaha.blade.php
+++ resources/views/web/input-data/usaha.blade.php
@@ -2,6 +2,10 @@
 
 @section('main-page')
 <div class="container-fluid">
-
+    <div class="card">
+        <div class="card-content">
+            <h5 class="card-title">Form Data Informasi tambahan & Usaha</h5>
+        </div>
+    </div>
 </div>
 @endsection
